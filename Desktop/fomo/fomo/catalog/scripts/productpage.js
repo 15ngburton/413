a4ff864e03c5d3894cedb19b89d2b10021694d96
15ngburton <15ngburton@gmail.com>
2018-03-19 13:23:26 -0600

    Now the thumbnails are working!

--- Desktop/fomo/fomo/catalog/scripts/productpage.js
+++ Desktop/fomo/fomo/catalog/scripts/productpage.js
@@ -1,6 +1,11 @@
-$(function () {
-  console.log("THIS");
-  $(".thumbNail").onmouseover = function(){
-    console.log(this);
-  };
+$(function(context) {
+  return function() {
+    console.log("THIS TEXT");
+    var thumbnails = document.getElementsByClassName("thumbnail");
+    for (i = 0; i < thumbnails.length; i++) {
+      thumbnails[i].onmouseover = function() {
+        document.getElementById("largeImage").setAttribute("src", this.getAttribute("src"))
+      }
+    }
+  }
 }(DMP_CONTEXT.get()))
